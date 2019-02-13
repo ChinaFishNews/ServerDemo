3492cb1376e381b77344741f6c3c1f80d0ea7d55
yuxinwen <yuxinwen@baidu.com>
2019-02-13 15:11:03 +0800

    Initial Commit

---
+++ ServerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ServerDemo
+//
+//  Created by xinwen on 2019/2/13.
+//  Copyright © 2019 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ServerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ServerDemo
+//
+//  Created by xinwen on 2019/2/13.
+//  Copyright © 2019 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
